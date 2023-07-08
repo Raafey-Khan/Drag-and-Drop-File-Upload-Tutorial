1205e0157ac11809756648e93405311348867fa8
Raafey-Khan <loyaalraafay@gmail.com>
2023-07-09 02:39:09 +0530

    After a Long Updated script.js

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
 const dragArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('.header');
 
+let button = document.querySelector('.button');
+let input = document.querySelector('input');
+
 let file;
-// when file is inside the drag area
 
-
-dragArea.addEventListener('dragover',(event) => {
-    event.preventDefault();
-    dragText.textContent = 'Release to Upload';
-    dragArea.classList.add('active');
-    //console.log('File is inside the drag area');
-
+button.onclick = () => {
+    input.click();
+};
+// when browse
+input.addEventListener('change', function(){
+    file = this.files[0];
+    console.log(file)
+})
+// When file is inside the drag area
+dragArea.addEventListener('dragover', (event) => {
+  event.preventDefault();
+  dragText.textContent = 'Release to Upload';
+  dragArea.classList.add('active');
 });
 
-
-// when file leaves the drag area
-
+// When file leaves the drag area
 dragArea.addEventListener('dragleave', () => {
-    dragText.textContent = 'Drag & Drop';
-    dragArea.classList.add('active');
-    //console.log('File left the drag area');
+  dragText.textContent = 'Drag & Drop';
+  dragArea.classList.remove('active');
 });
 
-// when the file is dropped in the drag area
+// When the file is dropped in the drag area
 dragArea.addEventListener('drop', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    file = event.dataTransfer.files[0];
+  file = event.dataTransfer.files[0];
+  let fileType = file.type;
 
-    let fileType = file.type;
-    //console.log(fileType);
-    //console.log(file)
-    //console.log('The file is dropped in drag area');
+  let validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf'];
+  if (validExtensions.includes(fileType)) {
+    let fileReader = new FileReader();
 
-    let validExtensions = ['image/jpeg','image/jpg','image/png','image/pdf'];
-    if(validExtensions.includes(fileType)){
-        let fileReader = new FileReader();
+    fileReader.onload = () => {
+      let fileURL = fileReader.result;
+      let imgTag = `<img src="${fileURL}" alt="" />`;
+      dragArea.innerHTML = imgTag;
+    };
 
-        fileReader.onload = () => {
-            let fileURL = fileReader.result;
-            //console.log(fileURL);
-            let imgTag = `<img src="${fileURL}" alt=""`;
-            dragArea.innerHTML = imgTag;
-        };
-        fileReader.readAsDataURL(file);
-    } else {
-        alert('This file is not an Image')
-    }
+    fileReader.readAsDataURL(file);
+  } else {
+    alert('This file is not an Image or PDF');
+  }
+
+  dragText.textContent = 'Drag & Drop';
+  dragArea.classList.remove('active');
 });
-
